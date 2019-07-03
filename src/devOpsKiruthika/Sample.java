3715259cfd6cb49c4c98827544cbb6097502a36d
kiruthikasekar <kiruthika.sekar@mindtree.com>
2019-07-03 21:10:24 +0530

    Added by Kiruthika

---
+++ src/devOpsKiruthika/Sample.java
@@ -0,0 +1,11 @@
+package devOpsKiruthika;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to devops 301");
+		System.out.println("Hello Kiruthika ....");
+
+	}
+
+}
